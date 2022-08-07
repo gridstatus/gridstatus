b86c40583bdfef99cab041455a52c0ef94bc630c
Max Kanter <kmax12@gmail.com>
2022-08-07 12:40:22 -0500

    change data source for nyiso demand data

--- isodata/nyiso.py
+++ isodata/nyiso.py
@@ -37,22 +37,7 @@ class NYISO(ISOBase):
         return self._yesterday_from_historical(self.get_historical_fuel_mix)
 
     def get_historical_fuel_mix(self, date):
-        date = isodata.utils._handle_date(date)
-        month = date.strftime('%Y%m01')
-        day = date.strftime('%Y%m%d')
-
-        file = f"{day}rtfuelmix.csv"
-        csv_url = f'http://mis.nyiso.com/public/csv/rtfuelmix/{file}'
-        zip_url = f'http://mis.nyiso.com/public/csv/rtfuelmix/{month}rtfuelmix_csv.zip'
-
-        # the last 7 days of file are hosted directly as csv
-        try:
-            mix_df = pd.read_csv(csv_url)
-        except:
-            r = requests.get(zip_url)
-            z = ZipFile(io.BytesIO(r.content))
-            mix_df = pd.read_csv(z.open(file))
-
+        mix_df = _download_nyiso_archive(date, "rtfuelmix")
         mix_df = mix_df.pivot_table(index="Time Stamp",
                                     columns="Fuel Category", values="Gen MW", aggfunc="first").reset_index()
 
@@ -76,25 +61,13 @@ class NYISO(ISOBase):
 
     def get_historical_demand(self, date):
         """Returns demand at a previous date in 5 minute intervals"""
-        date = isodata.utils._handle_date(date)
+        data = _download_nyiso_archive(date, "pal")
 
-        url = "http://dss.nyiso.com/dss_oasis/PublicReports"
-        data = {
-            'reportKey': 'RT_ACT_LOAD',
-            'startDate': date.strftime('%m/%d/%Y'),
-            'endDate': date.strftime('%m/%d/%Y'),
-            'version': 'L',  # latest versions
-            'dataFormat': 'CSV',
-            'filter': ['CAPITL', 'CENTRL', 'DUNWOD', 'GENESE', 'H Q', 'HUD VL', 'LONGIL', 'MHK VL', 'MILLWD', 'N.Y.C.', 'NORTH', 'NPX', 'O H', 'PJM', 'WEST']
-        }
-        r = requests.post(url, data=data)
+        demand = data.groupby("Time Stamp")[
+            "Load"].sum().reset_index()
 
-        data = pd.read_csv(io.StringIO(r.content.decode("utf8")))
-        demand = data.groupby("RTD End Time Stamp")[
-            "RTD Actual Load"].sum().reset_index()
-
-        demand = demand.rename(columns={"RTD End Time Stamp": "Time",
-                                        "RTD Actual Load": "Demand"})
+        demand = demand.rename(columns={"Time Stamp": "Time",
+                                        "Load": "Demand"})
 
         demand["Time"] = pd.to_datetime(
             demand["Time"]).dt.tz_localize(self.default_timezone)
@@ -124,6 +97,26 @@ class NYISO(ISOBase):
     # def get_day_ahead_prices(self,)
 
     # https://www.nyiso.com/energy-market-operational-data
+
+
+def _download_nyiso_archive(date, filename):
+    date = isodata.utils._handle_date(date)
+    month = date.strftime('%Y%m01')
+    day = date.strftime('%Y%m%d')
+
+    file = f"{day}{filename}.csv"
+    csv_url = f'http://mis.nyiso.com/public/csv/{filename}/{file}'
+    zip_url = f'http://mis.nyiso.com/public/csv/{filename}/{month}{filename}_csv.zip'
+
+    # the last 7 days of file are hosted directly as csv
+    try:
+        df = pd.read_csv(csv_url)
+    except:
+        r = requests.get(zip_url)
+        z = ZipFile(io.BytesIO(r.content))
+        df = pd.read_csv(z.open(file))
+
+    return df
 
 
 """
