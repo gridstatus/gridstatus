2e6d7c5f4348989a10461c8e9dee6df6a80172e4
Max Kanter <kmax12@gmail.com>
2022-10-20 14:00:40 -0500

    test miso and ercot files

---
+++ isodata/tests/test_miso.py
@@ -0,0 +1,7 @@
+import isodata
+
+
+def test_miso_locations():
+    iso = isodata.MISO()
+    data = iso.get_latest_lmp(Markets.REAL_TIME_5_MIN, locations=iso.hubs)
+    assert set(data["Location"].unique()) == set(iso.hubs)
