6a7381935d8200c4a84699f670b28a7ccdb4f5b1
Max Kanter <kmax12@gmail.com>
2022-11-01 09:47:49 -0500

    NYISO add `get_generators` and `get_loads` (#73)
    * add get generators
    
    * add get loads
    
    * update ercot for dst inference

--- gridstatus/nyiso.py
+++ gridstatus/nyiso.py
@@ -215,6 +215,44 @@ class NYISO(ISOBase):
 
         return df
 
+    def get_generators(self, verbose=False):
+        """Get a list of generators in NYISO
+
+        Parameters:
+            verbose (bool): print out requested url
+
+        Returns:
+            pd.DataFrame: a dataframe of generators and locations
+        """
+
+        url = "http://mis.nyiso.com/public/csv/generator/generator.csv"
+
+        if verbose:
+            print(f"Requesting {url}")
+
+        df = pd.read_csv(url)
+
+        return df
+
+    def get_loads(self, verbose=False):
+        """Get a list of loads in NYISO
+
+        Parameters:
+            verbose (bool): print out requested url
+
+        Returns:
+            pd.DataFrame: a dataframe of loads and locations
+        """
+
+        url = "http://mis.nyiso.com/public/csv/load/load.csv"
+
+        if verbose:
+            print(f"Requesting {url}")
+
+        df = pd.read_csv(url)
+
+        return df
+
     def _set_marketname(self, market: Markets) -> str:
         if market == Markets.REAL_TIME_5_MIN:
             marketname = "realtime"
