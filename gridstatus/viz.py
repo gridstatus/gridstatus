effcd065df06b6ef6d2391297721839e718e3fe5
Max Kanter <kmax12@gmail.com>
2022-12-22 14:07:11 -0600

    make heatmap two words

--- gridstatus/viz.py
+++ gridstatus/viz.py
@@ -1,27 +1,27 @@
 import plotly.express as px
 
 
-def dam_heatmap(df):
-    """Create a heatmap of day-ahead location marginal prices.
+def dam_heat_map(df):
+    """Create a heat map of day-ahead location marginal prices.
 
     Args:
         df (pd.DataFrame): A DataFrame with columns "Time", "Location", and "LMP".
 
     Returns:
-        plotly.graph_objects.Figure: A heatmap of day-ahead location marginal prices.
+        plotly.graph_objects.Figure: A heat map of day-ahead location marginal prices.
     """
 
     date_str = df["Time"].dt.date[0].strftime("%m/%d/%Y")
     title = "Day-Ahead Location Marginal Prices on " + date_str + " ($/MWh)"
 
-    heatmap_data = df.pivot(
+    heat_map_data = df.pivot(
         index="Location",
         columns="Hour",
         values="LMP",
     ).round(0)
 
     fig = px.imshow(
-        heatmap_data,
+        heat_map_data,
         text_auto=True,
         title=title,
         template="plotly_dark",
