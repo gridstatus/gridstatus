98fc33b8037714abf285736cf21fc06e116b6062
Jerry Chen <jerry@apache.org>
2022-12-28 08:18:58 -0600

    Remove unused imports in utils

--- gridstatus/utils.py
+++ gridstatus/utils.py
@@ -1,7 +1,6 @@
 import glob
 import io
 import os
-from urllib.parse import quote
 from zipfile import ZipFile
 
 import pandas as pd
@@ -9,7 +8,7 @@ import requests
 import tqdm
 
 import gridstatus
-from gridstatus.base import ISOBase, Markets, NotSupported, _interconnection_columns
+from gridstatus.base import Markets, NotSupported, _interconnection_columns
 from gridstatus.caiso import CAISO
 from gridstatus.ercot import Ercot
 from gridstatus.isone import ISONE
