ff74d9974c22eda6acde779e31ce0ed43966afa0
Jerry Chen <jerry@apache.org>
2022-12-29 12:46:10 -0600

    httpio: PickleCacheAdapter

---
+++ gridstatus/httpio/adapters/pickle_cache.py
@@ -0,0 +1,60 @@
+import hashlib
+import os
+import pickle
+import sys
+
+from gridstatus.httpio.adapters.base import BaseAdapter
+
+
+class PickleCacheAdapter(BaseAdapter):
+    def __init__(self, tmp_dir="/tmp"):
+        super().__init__("pickle_cache")
+        self.tmp_dir = tmp_dir
+
+    ALLOWED_METHODS = (
+        "get",
+        "post",
+        "read_csv",
+        "read_excel",
+        "read_html",
+    )
+
+    def before_filter(self, method, args, kwargs):
+        if method in self.ALLOWED_METHODS:
+            path = self._get_path(method, args, kwargs)
+            if os.path.exists(path):
+                return pickle.load(open(path, "rb"))
+        else:
+            print(
+                f"WARN: PickleCacheAdapter before_hook: method {method} not allowed",
+                file=sys.stderr,
+            )
+
+    def after_hook(self, method, args, kwargs, value, is_new_value=False):
+        if method in self.ALLOWED_METHODS:
+            if is_new_value:
+                path = self._get_path(method, args, kwargs)
+                with open(path, "wb") as f:
+                    pickle.dump(value, f)
+        else:
+            print(
+                f"WARN: PickleCacheAdapter after_hook: method {method} not allowed",
+                file=sys.stderr,
+            )
+
+    def _get_path(self, method, args, kwargs):
+        hash = PickleCacheAdapter._get_hash(method, args, kwargs)
+        return f"{self.tmp_dir}/{hash}.dat"
+
+    @staticmethod
+    def _get_hash(method, args, kwargs):
+        contents = repr(
+            sorted(
+                {
+                    "method": method,
+                    "args": args,
+                    "kwargs": sorted(kwargs),
+                }.items(),
+            ),
+        )
+        return hashlib.sha256(contents.encode("utf-8")).hexdigest()
--- gridstatus/httpio/auto_adapter_dispatcher.py
+++ gridstatus/httpio/auto_adapter_dispatcher.py
@@ -2,6 +2,7 @@ import os
 
 from gridstatus.httpio.adapter_dispatcher import AdapterDispatcher
 from gridstatus.httpio.adapters.logger import LoggerAdapter
+from gridstatus.httpio.adapters.pickle_cache import PickleCacheAdapter
 
 
 class AutoAdapterDispatcher(AdapterDispatcher):
@@ -9,3 +10,5 @@ class AutoAdapterDispatcher(AdapterDispatcher):
         super().__init__()
         if "HTTPIO_LOGGING" in os.environ:
             self.register_adapter(LoggerAdapter())
+        if "HTTPIO_PICKLE_CACHE" in os.environ:
+            self.register_adapter(PickleCacheAdapter())
