f0fd3b22828a174db449fc3a24ea533edd501031
Jerry Chen <jerry@apache.org>
2022-12-29 12:42:08 -0600

    httpio: BaseAdapter gets before_filter and after_hook

--- gridstatus/httpio/adapters/base.py
+++ gridstatus/httpio/adapters/base.py
@@ -1,3 +1,12 @@
 class BaseAdapter:
+    def __init__(self, name):
+        self.name = name
+
     def before_hook(self, method, args, kwargs):
+        pass
+
+    def before_filter(self, method, args, kwargs):
+        pass
+
+    def after_hook(self, method, args, kwargs, value, is_new_value=False):
         pass
