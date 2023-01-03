2c40bfc68cc535a4525e2fe15638f6e4ad6c8e45
Gaurav Sheni <gvsheni@gmail.com>
2023-01-02 21:21:27 -0500

    more docstring fixes

--- gridstatus/pjm.py
+++ gridstatus/pjm.py
@@ -234,19 +234,19 @@ class PJM(ISOBase):
             end (datetime.date, str): end date to get LMPs for
 
             market (str):  Supported Markets:
-            REAL_TIME_5_MIN, REAL_TIME_HOURLY, DAY_AHEAD_HOURLY
+                REAL_TIME_5_MIN, REAL_TIME_HOURLY, DAY_AHEAD_HOURLY
 
             locations (list, optional):  list of pnodeid to get LMPs for.
-            Defaults to "hubs". Use get_pnode_ids() to get
-            a list of possible pnode ids. If "all", will
-            return data from all p nodes (warning there are
-            over 10,000 unique pnodes, so expect millions or billions of rows!)
+                Defaults to "hubs". Use get_pnode_ids() to get
+                a list of possible pnode ids. If "all", will
+                return data from all p nodes (warning there are
+                over 10,000 unique pnodes, so expect millions or billions of rows!)
 
             location_type (str, optional):  If specified,
-            will only return data for nodes of this type.
-            Defaults to None. Possible location types are: 'ZONE',
-            'LOAD', 'GEN', 'AGGREGATE', 'INTERFACE', 'EXT',
-            'HUB', 'EHV', 'TIE', 'RESIDUAL_METERED_EDC'.
+                will only return data for nodes of this type.
+                Defaults to None. Possible location types are: 'ZONE',
+                'LOAD', 'GEN', 'AGGREGATE', 'INTERFACE', 'EXT',
+                'HUB', 'EHV', 'TIE', 'RESIDUAL_METERED_EDC'.
 
         """
         market = Markets(market)
