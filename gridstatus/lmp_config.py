8d6264f50b0e9a7f371aaefbfd4484e56e18d9d8
Jerry Chen <jerry@apache.org>
2023-01-12 13:41:13 -0600

    lmp_config: Use copies of __handle_date and __is_today from utils. Handle string dates

--- gridstatus/lmp_config.py
+++ gridstatus/lmp_config.py
@@ -3,7 +3,6 @@ import functools
 import pandas as pd
 
 from gridstatus.base import ISOBase, Markets, NotSupported
-from gridstatus.utils import _handle_date, is_today
 
 
 class lmp_config:
@@ -42,13 +41,10 @@ class lmp_config:
 
     @staticmethod
     def _parse_date(date, tz):
-        if date in (
-            "latest",
-            "today",
-        ):
-            return _handle_date("today", tz=tz)
-        elif isinstance(date, pd.Timestamp):
-            final_date = _handle_date(date, tz=tz)
+        if date == "latest":
+            return lmp_config.__handle_date("today", tz=tz)
+        elif isinstance(date, str) or isinstance(date, pd.Timestamp):
+            final_date = lmp_config.__handle_date(date, tz=tz)
             if not isinstance(final_date, pd.Timestamp):
                 raise ValueError(f"Cannot parse date {repr(date)}")
             return final_date
@@ -141,7 +137,7 @@ class lmp_config:
             "today",
         ):
             supported = original_date_arg in self.supports[market_arg]
-        elif is_today(date_arg, tz):
+        elif lmp_config.__is_today(date_arg, tz):
             supported = "today" in self.supports[market_arg]
         else:
             supported = "historical" in self.supports[market_arg]
@@ -150,3 +146,25 @@ class lmp_config:
             raise NotSupported(
                 f"{market_arg} does not support {repr(original_date_arg)}",
             )
+
+    # copied from utils.py to avoid circular import
+    @staticmethod
+    def __handle_date(date, tz=None):
+        if date == "today":
+            date = pd.Timestamp.now(tz=tz)
+
+        if not isinstance(date, pd.Timestamp):
+            date = pd.to_datetime(date)
+
+        if tz and date.tzinfo is None:
+            date = date.tz_localize(tz)
+
+        return date
+
+    # copied from utils.py to avoid circular import
+    @staticmethod
+    def __is_today(date, tz):
+        return (
+            lmp_config.__handle_date(date, tz=tz).date()
+            == pd.Timestamp.now(tz=tz).date()
+        )
