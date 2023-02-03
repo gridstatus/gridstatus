0fe5c6a607193289aa3013666454a48595c33fee
Max Kanter <kmax12@gmail.com>
2023-02-02 19:53:54 -0600

    Add PJM preliminary 5 Minute market (#144)
    * real time working
    
    * clean up
    
    * verbose

--- gridstatus/tests/test_pjm.py
+++ gridstatus/tests/test_pjm.py
@@ -49,19 +49,19 @@ class TestPJM(BaseTestISO):
         Markets.REAL_TIME_5_MIN,
     )
     def test_get_lmp_latest(self, market):
-        if market in [Markets.REAL_TIME_5_MIN, Markets.REAL_TIME_HOURLY]:
+        if market in [Markets.DAY_AHEAD_HOURLY, Markets.REAL_TIME_HOURLY]:
             with pytest.raises(NotSupported):
                 super().test_get_lmp_latest(market=market)
         else:
             super().test_get_lmp_latest(market=market)
 
     @with_markets(
-        Markets.DAY_AHEAD_HOURLY,
         Markets.REAL_TIME_HOURLY,
         Markets.REAL_TIME_5_MIN,
+        Markets.DAY_AHEAD_HOURLY,
     )
     def test_get_lmp_today(self, market):
-        if market in [Markets.REAL_TIME_5_MIN, Markets.REAL_TIME_HOURLY]:
+        if market in [Markets.REAL_TIME_HOURLY]:
             with pytest.raises(RuntimeError, match="No data found for query"):
                 super().test_get_lmp_today(market=market)
         else:
