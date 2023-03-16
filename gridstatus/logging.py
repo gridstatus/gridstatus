3d390236144a9e78e7619f3a7681352635044603
Gaurav Sheni <gvsheni@gmail.com>
2023-03-16 00:00:56 -0400

    fix based on feedback

--- gridstatus/logging.py
+++ gridstatus/logging.py
@@ -1,23 +1,5 @@
-import logging
-import sys
-
-
-def configure_logging():
-    logger = logging.getLogger("gridstatus")
-    if not len(logger.handlers):
-        logger.setLevel(logging.INFO)
-        fmt = ("%(name)s: %(levelname)s: %(message)s",)
-        # by default, StreamHandler logs to stderr
-        handler = logging.StreamHandler(sys.stderr)
-        # log info, debug, warning, error, critical to stdout
-        handler.setLevel(logging.INFO)
-        handler.setFormatter(logging.Formatter(fmt=fmt))
-        logger.addHandler(handler)
-    return logger
-
-
 def log(msg, verbose=False):
+    # TODO: use logging
     if verbose is False:
         return
-    logger = configure_logging()
-    logger.info(msg=msg)
+    print(msg)
--- gridstatus/tests/test_logger.py
+++ gridstatus/tests/test_logger.py
@@ -11,3 +11,8 @@ def msg():
 def test_log_stdout(msg, caplog):
     log(msg, verbose=True)
     assert msg, "\n" in caplog.text
+
+
+def test_log_no_stdout(msg, caplog):
+    log(msg, verbose=False)
+    assert msg, "\n" not in caplog.text
