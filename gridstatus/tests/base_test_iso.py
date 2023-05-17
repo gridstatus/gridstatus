db333ef89124a9dfcfd13759f9ac41af1e855401
Max Kanter <kmax12@gmail.com>
2023-05-17 10:38:14 -0500

    Frequency fix (#220)
    * update date range handling
    
    * disable test
    
    * fix nyiso
    
    * more nyiso fixes
    
    * add tests
    
    * remove debug
    
    * add eia api key for tests
    
    * change log
    
    * fix tests
    
    * fix tests

--- gridstatus/tests/base_test_iso.py
+++ gridstatus/tests/base_test_iso.py
@@ -85,11 +85,14 @@ class BaseTestISO:
             "today",
             self.iso.default_timezone,
         ) - pd.Timedelta(days=1)
-        yesterday = yesterday.replace(hour=0, minute=5, second=0, microsecond=0)
-        start = yesterday - pd.Timedelta(minutes=10)
-        df = self.iso.get_fuel_mix(start=start, end=yesterday)
+        yesterday = yesterday.replace(hour=1, minute=0, second=0, microsecond=0)
+        start = yesterday - pd.Timedelta(hours=3)
+        
+        # add one minute since pjm is exclusive of end date
+        # and does not include the whole day like other isos
+        df = self.iso.get_fuel_mix(start=start, end=yesterday + pd.Timedelta(minutes=1))
 
-        assert df["Time"].max() >= yesterday
+        assert df["Time"].max() >= yesterday.replace(hour=0, minute=0, second=0)
         assert df["Time"].min() <= start
 
         self._check_fuel_mix(df)
@@ -102,7 +105,8 @@ class BaseTestISO:
         start = yesterday.replace(hour=0, minute=5, second=0, microsecond=0)
         end = yesterday.replace(hour=6, minute=5, second=0, microsecond=0)
         df = self.iso.get_fuel_mix(start=start, end=end)
-        assert df["Time"].dt.date.unique().tolist() == [yesterday.date()]
+        # ignore last row, since it is sometime midnight of next day
+        assert df["Time"].iloc[:-1].dt.date.unique().tolist() == [yesterday.date()]
         self._check_fuel_mix(df)
 
     def test_get_fuel_mix_latest(self):
