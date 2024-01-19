7e0136505ae3d2fcfd104da4aa7b10b7c37937b9
Will Koehrsen <will.koehrsen@gmail.com>
2024-01-19 12:08:30 -0500

    Fix Ercot 15 Minute Load Time Handling (#319)
    * Fix Ercot 15 Minute Load Time Handling
    
    * Add tests
    
    * Add another test

--- gridstatus/tests/test_ercot.py
+++ gridstatus/tests/test_ercot.py
@@ -1,3 +1,5 @@
+from io import StringIO
+
 import pandas as pd
 import pytest
 
@@ -928,6 +930,73 @@ class TestErcot(BaseTestISO):
         # 1 Hour of data
         assert df.shape[0] == 4
         assert df.columns.tolist() == cols
+
+    def test_parse_doc_works_on_dst_data(self):
+        data_string = """DeliveryDate,TimeEnding,Demand,DSTFlag
+        03/13/2016,01:15,26362.1563,N
+        03/13/2016,01:30,26123.679,N
+        03/13/2016,01:45,25879.7454,N
+        03/13/2016,03:00,25668.166,N
+        """
+        # Read the data into a DataFrame
+        df = pd.read_csv(StringIO(data_string))
+
+        df = self.iso.parse_doc(df)
+
+        assert df["Interval Start"].min() == pd.Timestamp(
+            "2016-03-13 01:00:00-0600",
+            tz="US/Central",
+        )
+        assert df["Interval Start"].max() == pd.Timestamp(
+            "2016-03-13 01:45:00-0600",
+            tz="US/Central",
+        )
+
+        assert df["Interval End"].min() == pd.Timestamp(
+            "2016-03-13 01:15:00-0600",
+            tz="US/Central",
+        )
+        # Note the hour jump due to DST
+        assert df["Interval End"].max() == pd.Timestamp(
+            "2016-03-13 03:00:00-0500",
+            tz="US/Central",
+        )
+
+    def test_parse_doc_works_on_dst_end(self):
+        data_string = """DeliveryDate,TimeEnding,Demand,DSTFlag
+        11/06/2016,01:15,28907.1315,N
+        11/06/2016,01:30,28595.5918,N
+        11/06/2016,01:45,28266.6354,N
+        11/06/2016,01:00,28057.502,N
+        11/06/2016,01:15,27707.4798,Y
+        11/06/2016,01:30,27396.1973,Y
+        11/06/2016,01:45,27157.3464,Y
+        11/06/2016,02:00,26981.778,Y
+        """
+
+        df = pd.read_csv(StringIO(data_string))
+
+        df = self.iso.parse_doc(df)
+
+        assert df["Interval Start"].min() == pd.Timestamp(
+            "2016-11-06 00:45:00-0500",
+            tz="US/Central",
+        )
+
+        assert df["Interval Start"].max() == pd.Timestamp(
+            "2016-11-06 01:45:00-0600",
+            tz="US/Central",
+        )
+
+        assert df["Interval End"].min() == pd.Timestamp(
+            "2016-11-06 01:00:00-0500",
+            tz="US/Central",
+        )
+
+        assert df["Interval End"].max() == pd.Timestamp(
+            "2016-11-06 02:00:00-0600",
+            tz="US/Central",
+        )
 
     """get_lmp"""
 
