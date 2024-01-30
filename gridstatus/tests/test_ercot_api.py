d7ac97da3ce70b0a46754802bf0b11c356d0d3a6
Dan Morris <daniel.e.morris@gmail.com>
2024-01-29 21:16:48 -0800

    Loosen a test assertion to dodge time zone and data release timing issues

--- gridstatus/tests/test_ercot_api.py
+++ gridstatus/tests/test_ercot_api.py
@@ -1,6 +1,7 @@
 import datetime
 
 import pytest
+import pytz
 
 from gridstatus.ercot_api.api_parser import VALID_VALUE_TYPES, get_endpoints_map
 from gridstatus.ercot_api.ercot_api import hit_ercot_api
@@ -61,17 +62,20 @@ def test_hit_ercot_api():
 
     """
     Now a happy path test, using "actual system load by weather zone" endpoint.
-    Starting from two days ago should result in 48 hourly values, and there are
+    Starting from two days ago should result in 48 hourly values (or 24, depending on
+        when the data is released and when the test is run), and there are
         12 columns in the resulting dataframe.
     We are also testing here that datetime objects are correctly parsed into
         the desired date string format that the operatingDayFrom parameter expects.
     """
-    two_days_ago = datetime.datetime.now() - datetime.timedelta(days=2)
+    two_days_ago = datetime.datetime.now(tz=pytz.UTC) - datetime.timedelta(days=2)
     actual_by_wzn_endpoint = "/np6-345-cd/act_sys_load_by_wzn"
     two_days_actual_by_wzn = hit_ercot_api(
         actual_by_wzn_endpoint, operatingDayFrom=two_days_ago
     )
-    assert two_days_actual_by_wzn.shape == (48, 12)
+    result_rows, result_cols = two_days_actual_by_wzn.shape
+    assert result_rows in {24, 48}
+    assert result_cols == 12
 
     """
     Now let's apply a value filter and test it.
@@ -88,21 +92,21 @@ def test_hit_ercot_api():
         operatingDayFrom=two_days_ago,
         totalFrom=in_between_load,
     )
-    assert len(higher_loads_result["total"]) < 48
+    assert len(higher_loads_result["total"]) < result_rows
     assert all(higher_loads_result["total"] > in_between_load)
 
     """
     Now we test the page_size and max_pages arguments. We know that our two days
-        query returns 48 results, so if we lower page_size to 10 and max_pages to 3,
-        we should only get 30 rows total. We can also use this opportunity to test
-        that invalid parameter names are silently ignored.
+        query returns 24 or 48 results, so if we lower page_size to 10 and max_pages
+        to 2, we should only get 20 rows total. We can also use this opportunity to
+        test that invalid parameter names are silently ignored.
     """
     small_pages_result = hit_ercot_api(
         actual_by_wzn_endpoint,
         page_size=10,
-        max_pages=3,
+        max_pages=2,
         operatingDayFrom=two_days_ago,
         wowWhatAFakeParameter=True,
         thisOneIsAlsoFake=42,
     )
-    assert small_pages_result.shape == (30, 12)
+    assert small_pages_result.shape == (20, 12)
