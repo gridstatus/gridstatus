f966b7df67b6231422d5fb0997944e30a15ef82c
Max Kanter <kmax12@gmail.com>
2024-03-10 10:40:57 -0500

    add test for other dst switch

--- gridstatus/tests/test_decorators.py
+++ gridstatus/tests/test_decorators.py
@@ -9,3 +9,14 @@ def test_five_min_offset():
     before_dst = pd.Timestamp("2024-03-10 01:55:00", tz="US/Central")
     after = before_dst + FiveMinOffset()
     assert after == pd.Timestamp("2024-03-10 03:00:00", tz="US/Central")
+
+    # also test other direction
+
+    before_std = pd.Timestamp("2024-11-03 00:55:00", tz="US/Central") + pd.Timedelta(
+        hours=1,
+    )
+    after = before_std + FiveMinOffset()
+    assert after == pd.Timestamp("2024-11-03 00:55:00", tz="US/Central") + pd.Timedelta(
+        hours=1,
+        minutes=5,
+    )
