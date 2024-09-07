b300120250c46a24a6c8171268a860de939b172c
WillKoehrsen <will@cortexintel.com>
2024-09-07 14:24:23 -0400

    Use pd.NA instead of np.nan for string column

--- gridstatus/eia.py
+++ gridstatus/eia.py
@@ -766,7 +766,7 @@ def _handle_henry_hub_natural_gas_spot_prices(df):
         },
     )
 
-    df = df.replace({"NA": np.nan})
+    df = df.replace({"NA": pd.NA})
 
     df = utils.move_cols_to_front(df, ["Interval Start", "Interval End"])
 
