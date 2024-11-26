875ff19d0e6eb99d9d604ec4d44896e16413000a
Ryan Kladar <ryan@gridstatus.io>
2024-11-25 16:05:42 -0800

    feat(ENG 964) ISONE Hourly System Load (#490)
    * looking good
    
    * parameterize only the inputs rather than expected rows and things. easier to expand on
    
    * day start not hour start
    
    * base url more pythonic
    
    * fixture update
    
    * mutable defaults fixed and column name fixed
    
    * actually locid

--- gridstatus/tests/source_specific/test_isone_api.py
+++ gridstatus/tests/source_specific/test_isone_api.py
@@ -10,6 +10,17 @@ api_vcr = setup_vcr(
     record_mode=RECORD_MODE,
 )
 
+TEST_DATES = [
+    ("2024-11-02", "2024-11-04"),
+    ("2024-03-09", "2024-03-11"),
+]
+
+TEST_MULTIPLE_LOCATIONS = [
+    (".Z.MAINE", ".Z.NEWHAMPSHIRE"),
+]
+
+TEST_SINGLE_LOCATIONS = [loc for pair in TEST_MULTIPLE_LOCATIONS for loc in pair]
+
 
 class TestISONEAPI:
     def setup_class(cls):
@@ -37,25 +48,31 @@ class TestISONEAPI:
             "AreaType",
         ]
 
-    @api_vcr.use_cassette("test_get_realtime_hourly_demand_latest.yaml")
-    def test_get_realtime_hourly_demand_latest(self):
-        result = self.iso.get_realtime_hourly_demand(
-            date="latest",
-            locations=[".Z.MAINE"],
-        )
+    @pytest.mark.parametrize(
+        "location",
+        TEST_SINGLE_LOCATIONS,
+    )
+    def test_get_realtime_hourly_demand_latest(self, location: str):
+        with api_vcr.use_cassette(
+            f"test_get_realtime_hourly_demand_latest_{location}.yaml",
+        ):
+            result = self.iso.get_realtime_hourly_demand(
+                date="latest",
+                locations=[location],
+            )
 
-        assert isinstance(result, pd.DataFrame)
-        assert len(result) == 1
-        assert list(result.columns) == [
-            "Interval Start",
-            "Interval End",
-            "Location",
-            "Location Id",
-            "Load",
-        ]
-        assert result["Location"].iloc[0] == ".Z.MAINE"
-        assert result["Location Id"].iloc[0] == 4001
-        assert isinstance(result["Load"].iloc[0], (int, float))
+            assert isinstance(result, pd.DataFrame)
+            assert len(result) == 1
+            assert list(result.columns) == [
+                "Interval Start",
+                "Interval End",
+                "Location",
+                "Location Id",
+                "Load",
+            ]
+            assert result["Location"].iloc[0] == location
+            assert result["Location Id"].iloc[0] == ZONE_LOCATIONID_MAP[location]
+            assert isinstance(result["Load"].iloc[0], (int, float))
 
     @api_vcr.use_cassette("test_get_dayahead_hourly_demand_latest.yaml")
     def test_get_dayahead_hourly_demand_latest(self):
@@ -82,134 +99,124 @@ class TestISONEAPI:
         with pytest.raises(ValueError):
             self.iso.get_dayahead_hourly_demand(locations=["INVALID_LOCATION"])
 
-    @api_vcr.use_cassette("test_get_realtime_hourly_demand_multiple_locations.yaml")
-    def test_get_realtime_hourly_demand_multiple_locations(self):
-        result = self.iso.get_realtime_hourly_demand(
-            date="latest",
-            locations=[".Z.MAINE", ".Z.NEWHAMPSHIRE"],
-        )
+    @pytest.mark.parametrize(
+        "locations",
+        TEST_MULTIPLE_LOCATIONS,
+    )
+    def test_get_realtime_hourly_demand_multiple_locations(
+        self,
+        locations: tuple[str, str],
+    ):
+        with api_vcr.use_cassette(
+            f"test_get_realtime_hourly_demand_multiple_locations_{locations}.yaml",
+        ):
+            result = self.iso.get_realtime_hourly_demand(
+                date="latest",
+                locations=list(locations),
+            )
 
-        assert isinstance(result, pd.DataFrame)
-        assert len(result) == 2
-        assert list(result.columns) == [
-            "Interval Start",
-            "Interval End",
-            "Location",
-            "Location Id",
-            "Load",
-        ]
-        assert set(result["Location"]) == {".Z.MAINE", ".Z.NEWHAMPSHIRE"}
-        assert set(result["Location Id"]) == {4001, 4002}
-        assert all(isinstance(load, (int, float)) for load in result["Load"])
+            assert isinstance(result, pd.DataFrame)
+            assert len(result) == len(locations)
+            assert list(result.columns) == [
+                "Interval Start",
+                "Interval End",
+                "Location",
+                "Location Id",
+                "Load",
+            ]
+            assert set(result["Location"]) == set(locations)
+            assert set(result["Location Id"]) == {
+                ZONE_LOCATIONID_MAP[loc] for loc in locations
+            }
+            assert all(isinstance(load, (int, float)) for load in result["Load"])
 
     @pytest.mark.parametrize(
-        "date,end,locations,expected_rows,expected_location_ids",
-        [
-            (
-                "2024-10-06",
-                "2024-10-08",
-                [".Z.MAINE", ".Z.NEWHAMPSHIRE"],
-                96,
-                {4001, 4002},
-            ),
-        ],
+        "date,end,locations",
+        [(date, end, TEST_SINGLE_LOCATIONS) for date, end in TEST_DATES],
     )
-    @api_vcr.use_cassette("test_get_realtime_hourly_demand_date_range.yaml")
     def test_get_realtime_hourly_demand_date_range(
         self,
-        date,
-        end,
-        locations,
-        expected_rows,
-        expected_location_ids,
+        date: str,
+        end: str,
+        locations: list[str],
     ):
-        result = self.iso.get_realtime_hourly_demand(
-            date=date,
-            end=end,
-            locations=locations,
-        )
-
-        assert isinstance(result, pd.DataFrame)
-        assert len(result) == expected_rows
-        assert list(result.columns) == [
-            "Interval Start",
-            "Interval End",
-            "Location",
-            "Location Id",
-            "Load",
-        ]
-        assert set(result["Location"]) == set(locations)
-        assert set(result["Location Id"]) == expected_location_ids
-        assert all(isinstance(load, (int, float)) for load in result["Load"])
-        assert min(result["Interval Start"]).date() == pd.Timestamp(date).date()
-        assert max(result["Interval End"]).date() == pd.Timestamp(end).date()
-        for location in locations:
-            assert len(result[result["Location"] == location]) == expected_rows // len(
-                locations,
+        with api_vcr.use_cassette(
+            f"test_get_realtime_hourly_demand_date_range_{date}_{end}_{locations}.yaml",
+        ):
+            result = self.iso.get_realtime_hourly_demand(
+                date=date,
+                end=end,
+                locations=locations,
             )
 
+            assert isinstance(result, pd.DataFrame)
+            assert list(result.columns) == [
+                "Interval Start",
+                "Interval End",
+                "Location",
+                "Location Id",
+                "Load",
+            ]
+            assert set(result["Location"]) == set(locations)
+            assert set(result["Location Id"]) == {
+                ZONE_LOCATIONID_MAP[loc] for loc in locations
+            }
+            assert all(isinstance(load, (int, float)) for load in result["Load"])
+            assert min(result["Interval Start"]).date() == pd.Timestamp(date).date()
+            assert max(result["Interval End"]).date() == pd.Timestamp(end).date()
+
     @pytest.mark.parametrize(
-        "date,end,locations,expected_rows,expected_location_ids",
-        [
-            (
-                "2024-10-06",
-                "2024-10-08",
-                [".Z.MAINE", ".Z.NEWHAMPSHIRE"],
-                96,
-                {4001, 4002},
-            ),
-        ],
+        "date,end,locations",
+        [(date, end, TEST_SINGLE_LOCATIONS) for date, end in TEST_DATES],
     )
-    @api_vcr.use_cassette("test_get_dayahead_hourly_demand_date_range.yaml")
     def test_get_dayahead_hourly_demand_date_range(
         self,
-        date,
-        end,
-        locations,
-        expected_rows,
-        expected_location_ids,
+        date: str,
+        end: str,
+        locations: list[str],
     ):
-        result = self.iso.get_dayahead_hourly_demand(
-            date=date,
-            end=end,
-            locations=locations,
-        )
+        with api_vcr.use_cassette(
+            f"test_get_dayahead_hourly_demand_date_range_{date}_{end}_{locations}.yaml",
+        ):
+            result = self.iso.get_dayahead_hourly_demand(
+                date=date,
+                end=end,
+                locations=locations,
+            )
+
+            assert isinstance(result, pd.DataFrame)
+            assert list(result.columns) == [
+                "Interval Start",
+                "Interval End",
+                "Location",
+                "Location Id",
+                "Load",
+            ]
+            assert set(result["Location"]) == set(locations)
+            assert set(result["Location Id"]) == {
+                ZONE_LOCATIONID_MAP[loc] for loc in locations
+            }
+            assert all(isinstance(load, (int, float)) for load in result["Load"])
+            assert min(result["Interval Start"]).date() == pd.Timestamp(date).date()
+            assert max(result["Interval End"]).date() == pd.Timestamp(end).date()
+
+    @pytest.mark.parametrize(
+        "date,end",
+        TEST_DATES,
+    )
+    def test_get_hourly_load_forecast(self, date, end):
+        cassette_name = f"test_get_hourly_load_forecast_{date}_{end}.yaml"
+        with api_vcr.use_cassette(cassette_name):
+            result = self.iso.get_hourly_load_forecast(date=date, end=end)
 
         assert isinstance(result, pd.DataFrame)
-        assert len(result) == expected_rows
         assert list(result.columns) == [
             "Interval Start",
             "Interval End",
-            "Location",
-            "Location Id",
+            "Publish Time",
             "Load",
+            "Net Load",
         ]
-        assert set(result["Location"]) == set(locations)
-        assert set(result["Location Id"]) == expected_location_ids
-        assert all(isinstance(load, (int, float)) for load in result["Load"])
-        assert min(result["Interval Start"]).date() == pd.Timestamp(date).date()
-        assert max(result["Interval End"]).date() == pd.Timestamp(end).date()
-        for location in locations:
-            assert len(result[result["Location"] == location]) == expected_rows // len(
-                locations,
-            )
-
-    @pytest.mark.parametrize(
-        "date,end,expected_columns",
-        [
-            (
-                "2023-05-01",
-                "2023-05-03",
-                ["Interval Start", "Interval End", "Publish Time", "Load", "Net Load"],
-            ),
-        ],
-    )
-    @api_vcr.use_cassette("test_get_hourly_load_forecast.yaml")
-    def test_get_hourly_load_forecast(self, date, end, expected_columns):
-        result = self.iso.get_hourly_load_forecast(date=date, end=end)
-
-        assert isinstance(result, pd.DataFrame)
-        assert list(result.columns) == expected_columns
         assert (
             min(result["Interval Start"]).date()
             == pd.Timestamp(date).tz_localize(self.iso.default_timezone).date()
@@ -225,57 +232,53 @@ class TestISONEAPI:
         ).all()
 
     @pytest.mark.parametrize(
-        "date,end,expected_columns",
-        [
-            (
-                "2023-05-01",
-                "2023-05-03",
-                [
-                    "Interval Start",
-                    "Interval End",
-                    "Publish Time",
-                    "Location",
-                    "Load",
-                    "Regional Percentage",
-                ],
-            ),
-        ],
+        "date,end",
+        TEST_DATES,
     )
-    @api_vcr.use_cassette("test_get_reliability_region_load_forecast.yaml")
-    def test_get_reliability_region_load_forecast(self, date, end, expected_columns):
-        result = self.iso.get_reliability_region_load_forecast(date=date, end=end)
+    def test_get_reliability_region_load_forecast(self, date, end):
+        cassette_name = f"test_get_reliability_region_load_forecast_{date}_{end}.yaml"
+        with api_vcr.use_cassette(cassette_name):
+            result = self.iso.get_reliability_region_load_forecast(date=date, end=end)
 
-        assert isinstance(result, pd.DataFrame)
-        assert list(result.columns) == expected_columns
-        assert (
-            min(result["Interval Start"]).date()
-            == pd.Timestamp(date).tz_localize(self.iso.default_timezone).date()
-        )
-        assert max(result["Interval End"]) == pd.Timestamp(end).tz_localize(
-            self.iso.default_timezone,
-        )
-        assert (
-            (result["Interval End"] - result["Interval Start"]) == pd.Timedelta(hours=1)
-        ).all()
-        assert set(result["Location"].unique()) == {
-            ".Z.CONNECTICUT",
-            ".Z.MAINE",
-            ".Z.NEWHAMPSHIRE",
-            ".Z.RHODEISLAND",
-            ".Z.VERMONT",
-            ".Z.SEMASS",
-            ".Z.WCMASS",
-            ".Z.NEMASSBOST",
-        }
-        assert result["Load"].dtype in [np.int64, np.float64]
-        assert (result["Load"] > 0).all()
-        assert result["Regional Percentage"].dtype == np.float64
-        assert (
-            (result["Regional Percentage"] >= 0)
-            & (result["Regional Percentage"] <= 100)
-        ).all()
-        grouped = result.groupby(["Interval Start", "Publish Time"])
-        assert (grouped["Regional Percentage"].sum().between(99.9, 100.1)).all()
+            assert isinstance(result, pd.DataFrame)
+            assert list(result.columns) == [
+                "Interval Start",
+                "Interval End",
+                "Publish Time",
+                "Location",
+                "Load",
+                "Regional Percentage",
+            ]
+            assert (
+                min(result["Interval Start"]).date()
+                == pd.Timestamp(date).tz_localize(self.iso.default_timezone).date()
+            )
+            assert max(result["Interval End"]) == pd.Timestamp(end).tz_localize(
+                self.iso.default_timezone,
+            )
+            assert (
+                (result["Interval End"] - result["Interval Start"])
+                == pd.Timedelta(hours=1)
+            ).all()
+            assert set(result["Location"].unique()) == {
+                ".Z.CONNECTICUT",
+                ".Z.MAINE",
+                ".Z.NEWHAMPSHIRE",
+                ".Z.RHODEISLAND",
+                ".Z.VERMONT",
+                ".Z.SEMASS",
+                ".Z.WCMASS",
+                ".Z.NEMASSBOST",
+            }
+            assert result["Load"].dtype in [np.int64, np.float64]
+            assert (result["Load"] > 0).all()
+            assert result["Regional Percentage"].dtype == np.float64
+            assert (
+                (result["Regional Percentage"] >= 0)
+                & (result["Regional Percentage"] <= 100)
+            ).all()
+            grouped = result.groupby(["Interval Start", "Publish Time"])
+            assert (grouped["Regional Percentage"].sum().between(99.9, 100.1)).all()
 
     @api_vcr.use_cassette("test_get_fuel_mix_latest.yaml")
     def test_get_fuel_mix_latest(self):
@@ -293,26 +296,82 @@ class TestISONEAPI:
 
     @pytest.mark.parametrize(
         "date,end",
-        [
-            ("2024-01-01", "2024-01-03"),
-        ],
+        TEST_DATES,
     )
-    @api_vcr.use_cassette("test_get_fuel_mix_date_range.yaml")
     def test_get_fuel_mix_date_range(self, date, end):
-        result = self.iso.get_fuel_mix(date=date, end=end)
+        cassette_name = f"test_get_fuel_mix_{date}_{end}.yaml"
+        with api_vcr.use_cassette(cassette_name):
+            result = self.iso.get_fuel_mix(date=date, end=end)
 
-        assert isinstance(result, pd.DataFrame)
-        assert "Time" in result.columns
+            assert isinstance(result, pd.DataFrame)
+            assert "Time" in result.columns
 
-        assert min(result["Time"]).date() == pd.Timestamp(date).date()
-        assert max(result["Time"]).date() == pd.Timestamp(end).date() - pd.Timedelta(
-            days=1,
-        )
+            assert min(result["Time"]).date() == pd.Timestamp(date).date()
+            assert max(result["Time"]).date() == pd.Timestamp(
+                end,
+            ).date() - pd.Timedelta(
+                days=1,
+            )
 
-        assert all(isinstance(t, pd.Timestamp) for t in result["Time"])
-        numeric_cols = [col for col in result.columns if col != "Time"]
-        for col in numeric_cols:
-            assert result[col].dtype in [np.int64, np.float64]
-            assert (result[col] >= 0).all()
+            assert all(isinstance(t, pd.Timestamp) for t in result["Time"])
+            numeric_cols = [col for col in result.columns if col != "Time"]
+            for col in numeric_cols:
+                assert result[col].dtype in [np.int64, np.float64]
+                assert (result[numeric_cols].sum(axis=1) > 0).all()
 
-        assert (result[numeric_cols].sum(axis=1) > 0).all()
+    def test_get_load_hourly_latest(self):
+        with api_vcr.use_cassette("test_get_load_hourly_latest.yaml"):
+            result = self.iso.get_load_hourly(date="latest")
+
+            assert isinstance(result, pd.DataFrame)
+            assert len(result) == 1
+            assert list(result.columns) == [
+                "Interval Start",
+                "Interval End",
+                "Location",
+                "Location Id",
+                "Load",
+                "Native Load",
+                "ARD Demand",
+            ]
+            assert result["Location"].iloc[0] == "NEPOOL AREA"
+            assert result["Location Id"].iloc[0] == 32
+            assert isinstance(result["Load"].iloc[0], (int, float))
+            assert isinstance(result["Native Load"].iloc[0], (int, float))
+            assert isinstance(result["ARD Demand"].iloc[0], (int, float))
+
+    @pytest.mark.parametrize(
+        "date,end",
+        TEST_DATES,
+    )
+    def test_get_load_hourly_date_range(
+        self,
+        date,
+        end,
+    ):
+        cassette_name = f"test_get_load_hourly_{date}_{end}.yaml"
+
+        with api_vcr.use_cassette(cassette_name):
+            result = self.iso.get_load_hourly(date=date, end=end)
+
+            assert isinstance(result, pd.DataFrame)
+            assert list(result.columns) == [
+                "Interval Start",
+                "Interval End",
+                "Location",
+                "Location Id",
+                "Load",
+                "Native Load",
+                "ARD Demand",
+            ]
+            assert all(result["Location"] == "NEPOOL AREA")
+            assert all(result["Location Id"] == 32)
+            assert all(isinstance(load, (int, float)) for load in result["Load"])
+            assert all(isinstance(load, (int, float)) for load in result["Native Load"])
+            assert all(isinstance(load, (int, float)) for load in result["ARD Demand"])
+            assert min(result["Interval Start"]).date() == pd.Timestamp(date).date()
+            assert max(result["Interval End"]).date() == pd.Timestamp(end).date()
+            assert (
+                (result["Interval End"] - result["Interval Start"])
+                == pd.Timedelta(hours=1)
+            ).all()
