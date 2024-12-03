dcb0c3ede25c0c3f9830fa7df5ecaffe8c49e56b
Ryan Kladar <ryan@gridstatus.io>
2024-12-02 17:39:29 -0700

    feat(ENG 944) IESO Resource Adequacy Report (#482)
    * xmltodict is working nicely
    
    * xmltodict is working nicely
    
    * begin the pandas mashing
    
    * okay, certificates in an okay state
    
    * okay I believe this is working as intended
    
    * snapshot commit - I think we can generalize
    
    * okay, generalized a bit, now adding things back in
    
    * avoid adding multiple logging handlers
    
    * okay, demand side incoming
    
    * okay picking off demand sections one by one
    
    * okay picking off demand sections one by one
    
    * okay, working toward a general structure slowly, all 90 columns parsed and present
    
    * ignore fixtures for now
    
    * okay, this is decent for now
    
    * decent testing
    
    * dont blanket ignore json
    
    * refactor todo
    
    * some comments and todos
    
    * whoops, dont delete that
    
    * deps
    
    * include the beginning of a pandas-first approach, commented out, in case anyone sees something Im missing
    
    * snake case the data structure map
    
    * enable multiple file
    
    * adapt and fix tests
    
    * some more tests for the various ranges and vintage selections
    
    * pull out some reusable test params
    
    * add timezone and group date parsing
    
    * clean up logging verbosity and size
    
    * convert some to info where useful
    
    * eventually Ill circle back to logging consistently across the repo
    
    * logging needs another pass here to be configurable
    
    * excited to get logging where it should be...
    
    * remove mw from column header where applicable
    
    * simplify
    
    * record each time, for now
    
    * lock file
    
    * add since publish time functionality
    
    * handle unversioned file and invalid publish times
    
    * helpful logging
    
    * get filtered files not files
    
    * last modified as publish time is different
    
    * vintage
    
    * vintage, last modified, tests
    
    * docstring
    
    * last modified with passing tests

--- gridstatus/gs_logging.py
+++ gridstatus/gs_logging.py
@@ -1,26 +1,28 @@
 import logging
 
 
-def setup_gs_logger(level=logging.DEBUG):
+def setup_gs_logger(level: int = logging.DEBUG) -> logging.Logger:
     logger = logging.getLogger("gridstatus")
-    logger.setLevel(level)
-    handler = logging.StreamHandler()
-    handler.setLevel(level)  # Set handler level to the same as logger
-    handler.setFormatter(
-        logging.Formatter(
-            "%(asctime)s - %(levelname)s - %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S",
-        ),
-    )
-    logger.addHandler(handler)
+
+    if not logger.handlers:
+        logger.setLevel(level)
+        handler = logging.StreamHandler()
+        handler.setLevel(level)
+        handler.setFormatter(
+            logging.Formatter(
+                "%(asctime)s - %(levelname)s - %(message)s",
+                datefmt="%Y-%m-%d %H:%M:%S",
+            ),
+        )
+        logger.addHandler(handler)
+
     return logger
 
 
 logger = setup_gs_logger()
 
 
-def log(msg, verbose=False):
-    # TODO: use logging
+def log(msg: str, verbose: bool = False) -> None:
     if verbose is False:
         return
     print(msg)
--- gridstatus/tests/source_specific/test_ieso.py
+++ gridstatus/tests/source_specific/test_ieso.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pandas as pd
 import pytest
 from pandas.core.dtypes.common import is_numeric_dtype
@@ -10,8 +12,14 @@ from gridstatus.ieso import (
     MAXIMUM_DAYS_IN_PAST_FOR_LOAD,
 )
 from gridstatus.tests.base_test_iso import BaseTestISO
+from gridstatus.tests.vcr_utils import RECORD_MODE, setup_vcr
 
 TIME_COLUMN = "Interval Start"
+
+file_vcr = setup_vcr(
+    source="ieso",
+    record_mode=RECORD_MODE,
+)
 
 
 class TestIESO(BaseTestISO):
@@ -28,14 +36,17 @@ class TestIESO(BaseTestISO):
     def test_get_fuel_mix_date_or_start(self):
         pass
 
+    @pytest.mark.integration
     def test_get_fuel_mix_historical(self):
         super().test_get_fuel_mix_historical(time_column=TIME_COLUMN)
 
+    @pytest.mark.integration
     def test_get_fuel_mix_historical_with_date_range(self):
         super().test_get_fuel_mix_historical_with_date_range(
             time_column=TIME_COLUMN,
         )
 
+    @pytest.mark.integration
     def test_get_fuel_mix_range_two_days_with_day_start_endpoint(self):
         yesterday = utils._handle_date(
             "today",
@@ -53,6 +64,7 @@ class TestIESO(BaseTestISO):
         )
         assert df[TIME_COLUMN].min() <= start
 
+    @pytest.mark.integration
     def test_get_fuel_mix_start_end_same_day(self):
         yesterday = utils._handle_date(
             "today",
@@ -67,9 +79,11 @@ class TestIESO(BaseTestISO):
         ]
         self._check_fuel_mix(df)
 
+    @pytest.mark.integration
     def test_get_fuel_mix_latest(self):
         super().test_get_fuel_mix_latest(time_column=TIME_COLUMN)
 
+    @pytest.mark.integration
     def test_get_fuel_mix_in_future_raises_error(self):
         with pytest.raises(NotSupported):
             self.iso.get_fuel_mix(
@@ -79,6 +93,7 @@ class TestIESO(BaseTestISO):
 
     """get_generator_report_hourly"""
 
+    @pytest.mark.integration
     def test_get_generator_report_hourly_historical(self):
         # date string works
         date = pd.Timestamp.now(tz=self.default_timezone) - pd.Timedelta(days=10)
@@ -110,6 +125,7 @@ class TestIESO(BaseTestISO):
         assert df.loc[0][TIME_COLUMN].tz is not None
         self._check_get_generator_report_hourly(df)
 
+    @pytest.mark.integration
     def test_get_generator_report_hourly_historical_with_date_range(self):
         # range not inclusive, add one to include today
         num_days = 7
@@ -127,6 +143,7 @@ class TestIESO(BaseTestISO):
         # make sure right number of days are returned
         assert df[TIME_COLUMN].dt.day.nunique() == num_days
 
+    @pytest.mark.integration
     def test_get_generator_report_hourly_range_two_days_with_end(self):
         yesterday = utils._handle_date(
             "today",
@@ -149,6 +166,7 @@ class TestIESO(BaseTestISO):
 
         self._check_get_generator_report_hourly(df)
 
+    @pytest.mark.integration
     def test_get_generator_report_hourly_start_end_same_day(self):
         yesterday = utils._handle_date(
             "today",
@@ -163,6 +181,7 @@ class TestIESO(BaseTestISO):
         ]
         self._check_get_generator_report_hourly(df)
 
+    @pytest.mark.integration
     def test_get_generator_report_hourly_latest(self):
         df = self.iso.get_generator_report_hourly("latest")
         self._check_get_generator_report_hourly(df)
@@ -176,10 +195,12 @@ class TestIESO(BaseTestISO):
             # Account for data not immediately available
         ).floor("h") - pd.Timedelta(hours=2)
 
+    @pytest.mark.integration
     def test_get_generator_report_hourly_today(self):
         df = self.iso.get_generator_report_hourly("today")
         assert df.equals(self.iso.get_generator_report_hourly("latest"))
 
+    @pytest.mark.integration
     def test_get_generator_report_hourly_too_far_in_past_raises_error(self):
         with pytest.raises(NotSupported):
             self.iso.get_generator_report_hourly(
@@ -189,6 +210,7 @@ class TestIESO(BaseTestISO):
                 ),
             )
 
+    @pytest.mark.integration
     def test_get_generator_report_hourly_in_future_raises_error(self):
         with pytest.raises(NotSupported):
             self.iso.get_generator_report_hourly(
@@ -222,6 +244,7 @@ class TestIESO(BaseTestISO):
 
     """get_load"""
 
+    @pytest.mark.integration
     def test_get_load_today(self):
         df = self.iso.get_load("today")
         self._check_load(df)
@@ -234,6 +257,7 @@ class TestIESO(BaseTestISO):
 
         assert (df[TIME_COLUMN].dt.date == today.date()).all()
 
+    @pytest.mark.integration
     def test_get_load_latest(self):
         df = self.iso.get_load("latest")
 
@@ -244,6 +268,7 @@ class TestIESO(BaseTestISO):
 
         assert df.shape[0] <= 12
 
+    @pytest.mark.integration
     def test_get_load_yesterday_full_day(self):
         date = (
             pd.Timestamp.now(tz=self.default_timezone) - pd.Timedelta(days=1)
@@ -262,6 +287,7 @@ class TestIESO(BaseTestISO):
         end_of_date = beginning_of_date + pd.Timedelta(days=1)
         assert df["Interval End"].max() == end_of_date
 
+    @pytest.mark.integration
     def test_get_load_historical_with_date_range(self):
         num_days = 2
         end = pd.Timestamp.now(
@@ -278,6 +304,7 @@ class TestIESO(BaseTestISO):
 
         assert data_tuple.equals(data)
 
+    @pytest.mark.integration
     def test_get_load_historical(self):
         # pick a test date 2 weeks back
         test_date = (pd.Timestamp.now() - pd.Timedelta(days=14)).date()
@@ -303,6 +330,7 @@ class TestIESO(BaseTestISO):
             "%Y%m%d",
         ) == test_date.strftime("%Y%m%d")
 
+    @pytest.mark.integration
     def test_get_load_tomorrow_raises_error(self):
         with pytest.raises(NotSupported):
             self.iso.get_load(
@@ -310,6 +338,7 @@ class TestIESO(BaseTestISO):
                 + pd.Timedelta(days=1),
             )
 
+    @pytest.mark.integration
     def test_get_load_too_far_in_past_raises_error(self):
         with pytest.raises(NotSupported):
             self.iso.get_load(
@@ -319,6 +348,7 @@ class TestIESO(BaseTestISO):
 
     """get_load_forecast"""
 
+    @pytest.mark.integration
     def test_get_load_forecast_today(self):
         forecast = self.iso.get_load_forecast("today")
         self._check_load_forecast(forecast)
@@ -332,6 +362,7 @@ class TestIESO(BaseTestISO):
             tz=self.default_timezone,
         ).normalize() + pd.Timedelta(days=2)
 
+    @pytest.mark.integration
     def test_get_load_forecast_latest(self):
         assert self.iso.get_load_forecast("latest").equals(
             self.iso.get_load_forecast("today"),
@@ -347,11 +378,13 @@ class TestIESO(BaseTestISO):
 
     """get_zonal_load_forecast"""
 
+    @pytest.mark.integration
     def test_get_zonal_load_forecast_historical(self):
         test_date = (pd.Timestamp.now() - pd.Timedelta(days=3)).date()
         forecast = self.iso.get_zonal_load_forecast(date=test_date)
         self._check_zonal_load_forecast(forecast)
 
+    @pytest.mark.integration
     def test_get_zonal_load_forecast_historical_with_date_range(self):
         end = pd.Timestamp.now().normalize() - pd.Timedelta(days=1)
         start = (end - pd.Timedelta(days=2)).date()
@@ -361,6 +394,7 @@ class TestIESO(BaseTestISO):
         )
         self._check_zonal_load_forecast(forecast)
 
+    @pytest.mark.integration
     def test_get_zonal_load_forecast_today(self):
         forecast = self.iso.get_zonal_load_forecast("today")
 
@@ -383,6 +417,7 @@ class TestIESO(BaseTestISO):
 
         self._check_zonal_load_forecast(forecast)
 
+    @pytest.mark.integration
     def test_get_zonal_load_forecast_latest(self):
         assert self.iso.get_zonal_load_forecast("latest").equals(
             self.iso.get_zonal_load_forecast("today"),
@@ -390,6 +425,7 @@ class TestIESO(BaseTestISO):
 
     """get_status"""
 
+    @pytest.mark.integration
     def test_get_status_latest(self):
         # ensure there is a homepage if gridstatus can retrieve a status
         assert isinstance(self.iso.status_homepage, str)
@@ -509,3 +545,300 @@ class TestIESO(BaseTestISO):
             "SOLAR",
             "WIND",
         ]
+
+    """get_resource_adequacy_report"""
+
+    # NOTE(kladar): we will see how future data rolls in and historical rolls off
+    RESOURCE_ADEQUACY_TEST_DATES = [
+        (
+            (pd.Timestamp.now(tz=default_timezone) - pd.Timedelta(days=119)).strftime(
+                "%Y-%m-%d",
+            ),
+            (pd.Timestamp.now(tz=default_timezone) - pd.Timedelta(days=115)).strftime(
+                "%Y-%m-%d",
+            ),
+        ),
+        (
+            (pd.Timestamp.now(tz=default_timezone) - pd.Timedelta(days=3)).strftime(
+                "%Y-%m-%d",
+            ),
+            (pd.Timestamp.now(tz=default_timezone) - pd.Timedelta(days=1)).strftime(
+                "%Y-%m-%d",
+            ),
+        ),
+        (
+            (pd.Timestamp.now(tz=default_timezone)).strftime("%Y-%m-%d"),
+            (pd.Timestamp.now(tz=default_timezone) + pd.Timedelta(days=2)).strftime(
+                "%Y-%m-%d",
+            ),
+        ),
+        (
+            (pd.Timestamp.now(tz=default_timezone) + pd.Timedelta(days=1)).strftime(
+                "%Y-%m-%d",
+            ),
+            (pd.Timestamp.now(tz=default_timezone) + pd.Timedelta(days=3)).strftime(
+                "%Y-%m-%d",
+            ),
+        ),
+        (
+            (pd.Timestamp.now(tz=default_timezone) + pd.Timedelta(days=28)).strftime(
+                "%Y-%m-%d",
+            ),
+            (pd.Timestamp.now(tz=default_timezone) + pd.Timedelta(days=30)).strftime(
+                "%Y-%m-%d",
+            ),
+        ),
+    ]
+
+    REQUIRED_RESOURCE_ADEQUACY_COLUMNS = [
+        "Interval Start",
+        "Interval End",
+        "Publish Time",
+        "Forecast Supply Capacity",
+        "Forecast Supply Energy MWh",
+        "Forecast Supply Bottled Capacity",
+        "Forecast Supply Regulation",
+        "Total Forecast Supply",
+        "Total Requirement",
+        "Capacity Excess / Shortfall",
+        "Energy Excess / Shortfall MWh",
+        "Offered Capacity Excess / Shortfall",
+        "Resources Not Scheduled",
+        "Imports Not Scheduled",
+        "Nuclear Capacity",
+        "Nuclear Outages",
+        "Nuclear Offered",
+        "Nuclear Scheduled",
+        "Gas Capacity",
+        "Gas Outages",
+        "Gas Offered",
+        "Gas Scheduled",
+        "Hydro Capacity",
+        "Hydro Outages",
+        "Hydro Forecasted MWh",
+        "Hydro Offered",
+        "Hydro Scheduled",
+        "Wind Capacity",
+        "Wind Outages",
+        "Wind Forecasted",
+        "Wind Scheduled",
+        "Solar Capacity",
+        "Solar Outages",
+        "Solar Forecasted",
+        "Solar Scheduled",
+        "Biofuel Capacity",
+        "Biofuel Outages",
+        "Biofuel Offered",
+        "Biofuel Scheduled",
+        "Other Capacity",
+        "Other Outages",
+        "Other Offered/Forecasted",
+        "Other Scheduled",
+        "Manitoba Imports Offered",
+        "Manitoba Imports Scheduled",
+        "Minnesota Imports Offered",
+        "Minnesota Imports Scheduled",
+        "Michigan Imports Offered",
+        "Michigan Imports Scheduled",
+        "New York Imports Offered",
+        "New York Imports Scheduled",
+        "Quebec Imports Offered",
+        "Quebec Imports Scheduled",
+        "Total Internal Resources Outages",
+        "Total Internal Resources Offered/Forecasted",
+        "Total Internal Resources Scheduled",
+        "Total Imports Offers",
+        "Total Imports Scheduled",
+        "Total Imports Estimated",
+        "Total Imports Capacity",
+        "Manitoba Exports Offered",
+        "Manitoba Exports Scheduled",
+        "Minnesota Exports Offered",
+        "Minnesota Exports Scheduled",
+        "Michigan Exports Offered",
+        "Michigan Exports Scheduled",
+        "New York Exports Offered",
+        "New York Exports Scheduled",
+        "Quebec Exports Offered",
+        "Quebec Exports Scheduled",
+        "Total Exports Bids",
+        "Total Exports Scheduled",
+        "Total Exports Capacity",
+        "Total Operating Reserve",
+        "Minimum 10 Minute Operating Reserve",
+        "Minimum 10 Minute Spin OR",
+        "Load Forecast Uncertainties",
+        "Additional Contingency Allowances",
+        "Ontario Demand Forecast",
+        "Ontario Peak Demand",
+        "Ontario Average Demand",
+        "Ontario Wind Embedded Forecast",
+        "Ontario Solar Embedded Forecast",
+        "Ontario Dispatchable Load Capacity",
+        "Ontario Dispatchable Load Bid / Forecasted",
+        "Ontario Dispatchable Load Scheduled ON",
+        "Ontario Dispatchable Load Scheduled OFF",
+        "Ontario Hourly Demand Response Bid/Forecasted",
+        "Ontario Hourly Demand Response Scheduled",
+        "Ontario Hourly Demand Response Curtailed",
+        "Last Modified",
+    ]
+
+    @pytest.mark.parametrize(
+        "date",
+        [date[0] for date in RESOURCE_ADEQUACY_TEST_DATES],
+    )
+    def test_get_resource_adequacy_report_single_date_latest_report(
+        self,
+        date: str | datetime.date,
+    ):
+        with file_vcr.use_cassette(f"test_get_resource_adequacy_report_{date}.yaml"):
+            df = self.iso.get_resource_adequacy_report(date, vintage="latest")
+
+        assert isinstance(df, pd.DataFrame)
+        assert df.shape == (24, 91)  # 24 rows and 91 columns for each file
+        for col in self.REQUIRED_RESOURCE_ADEQUACY_COLUMNS:
+            assert col in df.columns
+
+        assert self._check_is_datetime_type(df["Interval Start"])
+        assert self._check_is_datetime_type(df["Interval End"])
+        assert self._check_is_datetime_type(df["Publish Time"])
+        assert (
+            (df["Interval End"] - df["Interval Start"]) == pd.Timedelta(hours=1)
+        ).all()
+
+    @pytest.mark.parametrize(
+        "date, end",
+        RESOURCE_ADEQUACY_TEST_DATES,
+    )
+    def test_get_latest_resource_adequacy_report_date_range(self, date: str, end: str):
+        with file_vcr.use_cassette(
+            f"test_get_latest_resource_adequacy_report_{date}_to_{end}.yaml",
+        ):
+            df = self.iso.get_resource_adequacy_report(date, end=end, vintage="latest")
+
+        assert isinstance(df, pd.DataFrame)
+        assert df.shape[1] == 91
+        for col in self.REQUIRED_RESOURCE_ADEQUACY_COLUMNS:
+            assert col in df.columns
+        expected_rows = ((pd.Timestamp(end) - pd.Timestamp(date)).days) * 24
+        assert df.shape[0] == expected_rows
+
+    @pytest.mark.parametrize(
+        "date, end",
+        RESOURCE_ADEQUACY_TEST_DATES,
+    )
+    def test_get_all_resource_adequacy_report_date_range(self, date: str, end: str):
+        with file_vcr.use_cassette(
+            f"test_get_all_resource_adequacy_report_{date}_to_{end}.yaml",
+        ):
+            df = self.iso.get_resource_adequacy_report(date, end=end, vintage="all")
+
+        assert isinstance(df, pd.DataFrame)
+        assert df.shape[1] == 91
+        for col in self.REQUIRED_RESOURCE_ADEQUACY_COLUMNS:
+            assert col in df.columns
+
+    # TODO(kladar): eventually don't record this each time
+    @file_vcr.use_cassette(
+        "test_get_latest_resource_adequacy_json.yaml",
+        record_mode="ALL",
+    )
+    def test_get_latest_resource_adequacy_json(self):
+        date = pd.Timestamp.now(tz=self.default_timezone)
+        json_data, last_modified = self.iso._get_latest_resource_adequacy_json(date)
+
+        assert isinstance(json_data, dict)
+        assert isinstance(last_modified, pd.Timestamp)
+        assert "Document" in json_data
+        assert "DocHeader" in json_data["Document"]
+        assert "DocBody" in json_data["Document"]
+
+        doc_body = json_data["Document"]["DocBody"]
+        assert "ForecastSupply" in doc_body
+        assert "ForecastDemand" in doc_body
+        assert "DeliveryDate" in doc_body
+
+    @file_vcr.use_cassette("test_get_all_resource_adequacy_json.yaml")
+    def test_get_all_resource_adequacy_json(self):
+        date = pd.Timestamp.now(tz=self.default_timezone)
+        json_data_with_times = self.iso._get_all_resource_adequacy_jsons(date)
+
+        assert isinstance(json_data_with_times, list)
+        for json_data, last_modified in json_data_with_times:
+            assert isinstance(json_data, dict)
+            assert isinstance(last_modified, pd.Timestamp)
+            assert "Document" in json_data
+            assert "DocHeader" in json_data["Document"]
+            assert "DocBody" in json_data["Document"]
+
+            doc_body = json_data["Document"]["DocBody"]
+            assert "ForecastSupply" in doc_body
+            assert "ForecastDemand" in doc_body
+            assert "DeliveryDate" in doc_body
+
+    def test_get_resource_adequacy_data_structure_map(self):
+        data_map = self.iso._get_resource_adequacy_data_structure_map()
+
+        assert isinstance(data_map, dict)
+        assert "supply" in data_map
+        assert "demand" in data_map
+
+        supply = data_map["supply"]
+        assert "hourly" in supply
+        assert "fuel_type_hourly" in supply
+        assert "total_internal_resources" in supply
+        assert "zonal_import_hourly" in supply
+
+        demand = data_map["demand"]
+        assert "ontario_demand" in demand
+        assert "zonal_export_hourly" in demand
+        assert "total_exports" in demand
+        assert "reserves" in demand
+
+    def test_extract_hourly_values(self):
+        test_data = {
+            "Capacities": {
+                "Capacity": [
+                    {"DeliveryHour": "1", "EnergyMW": "100"},
+                    {"DeliveryHour": "2", "EnergyMW": "200"},
+                    {"DeliveryHour": "3", "EnergyMW": "300"},
+                    {"DeliveryHour": "4", "EnergyMW": "400"},
+                    {"DeliveryHour": "5", "EnergyMW": "500"},
+                    {"DeliveryHour": "6", "EnergyMW": "600"},
+                    {"DeliveryHour": "7", "EnergyMW": "700"},
+                    {"DeliveryHour": "8", "EnergyMW": "800"},
+                    {"DeliveryHour": "9", "EnergyMW": "900"},
+                    {"DeliveryHour": "10", "EnergyMW": "1000"},
+                    {"DeliveryHour": "11", "EnergyMW": "1100"},
+                    {"DeliveryHour": "12", "EnergyMW": "1200"},
+                    {"DeliveryHour": "13", "EnergyMW": "1300"},
+                    {"DeliveryHour": "14", "EnergyMW": "1400"},
+                    {"DeliveryHour": "15", "EnergyMW": "1500"},
+                    {"DeliveryHour": "16", "EnergyMW": "1600"},
+                    {"DeliveryHour": "17", "EnergyMW": "1700"},
+                    {"DeliveryHour": "18", "EnergyMW": "1800"},
+                    {"DeliveryHour": "19", "EnergyMW": "1900"},
+                    {"DeliveryHour": "20", "EnergyMW": "2000"},
+                    {"DeliveryHour": "21", "EnergyMW": "2100"},
+                    {"DeliveryHour": "22", "EnergyMW": "2200"},
+                    {"DeliveryHour": "23", "EnergyMW": "2300"},
+                    {"DeliveryHour": "24", "EnergyMW": "2400"},
+                ],
+            },
+        }
+
+        report_data = []
+        self.iso._extract_hourly_values(
+            data=test_data,
+            path=["Capacities", "Capacity"],
+            column_name="Test Capacity",
+            value_key="EnergyMW",
+            report_data=report_data,
+        )
+
+        assert len(report_data) == 24
+        assert report_data[0]["DeliveryHour"] == 1
+        assert report_data[0]["Test Capacity"] == 100.0
+        assert report_data[1]["DeliveryHour"] == 2
+        assert report_data[1]["Test Capacity"] == 200.0
