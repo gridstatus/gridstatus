9911688b01fa4156a96f17d5e7dccdd8494ce57f
WillKoehrsen <will@cortexintel.com>
2025-01-08 11:40:36 -0500

    Fix tests

--- gridstatus/tests/source_specific/test_miso_api.py
+++ gridstatus/tests/source_specific/test_miso_api.py
@@ -97,13 +97,15 @@ class TestMISOAPI(TestHelperMixin):
     @pytest.mark.integration
     @api_vcr.use_cassette("test_get_lmp_real_time_5_min_ex_ante")
     def test_get_lmp_real_time_5_min_ex_ante_date_range(self):
-        start = self.local_now() - pd.DateOffset(days=2)
-        end = start + pd.Timedelta(hours=2)
+        start = (self.local_now() - pd.DateOffset(days=2)).floor("5min")
+        end = start + pd.Timedelta(hours=1)
         df = self.iso.get_lmp_real_time_5_min_ex_ante(start, end)
 
         self._check_lmp(df, market_value=Markets.REAL_TIME_5_MIN_EX_ANTE.value)
 
         assert df["Interval Start"].min() == start.floor("5min")
+        # Function is exclusive of the end time for interval start, but interval
+        # end will be the same as the end time
         assert df["Interval End"].max() == end.floor("5min")
 
     """get_lmp_real_time_5_min_ex_post_prelim"""
@@ -111,8 +113,8 @@ class TestMISOAPI(TestHelperMixin):
     @pytest.mark.integration
     @api_vcr.use_cassette("test_get_lmp_real_time_5_min_ex_post_prelim")
     def test_get_lmp_real_time_5_min_ex_post_prelim_date_range(self):
-        start = self.local_now() - pd.DateOffset(days=2)
-        end = start + pd.Timedelta(hours=2)
+        start = (self.local_now() - pd.DateOffset(days=2)).floor("5min")
+        end = start + pd.Timedelta(hours=1)
         df = self.iso.get_lmp_real_time_5_min_ex_post_prelim(start, end)
 
         self._check_lmp(df, market_value=Markets.REAL_TIME_5_MIN_EX_POST_PRELIM.value)
@@ -125,11 +127,11 @@ class TestMISOAPI(TestHelperMixin):
     @pytest.mark.integration
     @api_vcr.use_cassette("test_get_lmp_real_time_5_min_ex_post_final")
     def test_get_lmp_real_time_5_min_ex_post_final_date_range(self):
-        start = self.local_now() - pd.DateOffset(days=5)
-        end = start + pd.Timedelta(hours=2)
+        start = (self.local_now() - pd.DateOffset(days=5)).floor("5min")
+        end = start + pd.Timedelta(hours=1)
         df = self.iso.get_lmp_real_time_5_min_ex_post_final(start, end)
 
         self._check_lmp(df, market_value=Markets.REAL_TIME_5_MIN_EX_POST_FINAL.value)
 
-        assert df["Interval Start"].min() == start.floor("5min")
-        assert df["Interval End"].max() == end.floor("5min")
+        assert df["Interval Start"].min() == start
+        assert df["Interval End"].max() == end
