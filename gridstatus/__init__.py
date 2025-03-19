1590e6fc451683f5863088a0f248033b68ca3e73
Kladar <ryan@gridstatus.io>
2025-03-19 12:03:08 -0700

    init import

--- gridstatus/__init__.py
+++ gridstatus/__init__.py
@@ -19,7 +19,7 @@ import gridstatus.viz
 from gridstatus.utils import load_folder
 
 from gridstatus.nyiso import NYISO
-from gridstatus.caiso.caiso import CAISO
+from gridstatus.caiso import CAISO
 from gridstatus.ercot import Ercot
 from gridstatus.isone import ISONE
 from gridstatus.miso import MISO
