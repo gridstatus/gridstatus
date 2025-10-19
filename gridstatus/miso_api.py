484f5730b261770f476daffd542b345961ddc592
WillKoehrsen <will@gridstatus.io>
2025-10-18 21:27:18 -0500

    Move Decorator Back to Private Methods

--- gridstatus/miso_api.py
+++ gridstatus/miso_api.py
@@ -77,12 +77,12 @@ class MISOAPI:
         self.default_timezone = "EST"
         self.initial_sleep_seconds = initial_sleep_seconds
 
-    @support_date_range(frequency="HOUR_START")
     def get_lmp_day_ahead_hourly_ex_ante(
         self,
         date: str | pd.Timestamp | tuple[pd.Timestamp, pd.Timestamp],
         end: str | pd.Timestamp | tuple[pd.Timestamp, pd.Timestamp] | None = None,
         verbose: bool = False,
+        **kwargs,
     ) -> pd.DataFrame:
         return self._get_pricing_data(
             date,
@@ -91,14 +91,15 @@ class MISOAPI:
             market=Markets.DAY_AHEAD_HOURLY_EX_ANTE,
             version=EX_ANTE,
             verbose=verbose,
+            **kwargs,
         )
 
-    @support_date_range(frequency="HOUR_START")
     def get_lmp_day_ahead_hourly_ex_post(
         self,
         date: str | pd.Timestamp | tuple[pd.Timestamp, pd.Timestamp],
         end: str | pd.Timestamp | tuple[pd.Timestamp, pd.Timestamp] | None = None,
         verbose: bool = False,
+        **kwargs,
     ) -> pd.DataFrame:
         return self._get_pricing_data(
             date,
@@ -107,14 +108,15 @@ class MISOAPI:
             market=Markets.DAY_AHEAD_HOURLY_EX_POST,
             version=EX_POST,
             verbose=verbose,
+            **kwargs,
         )
 
-    @support_date_range(frequency="HOUR_START")
     def get_lmp_real_time_hourly_ex_post_prelim(
         self,
         date: str | pd.Timestamp | tuple[pd.Timestamp, pd.Timestamp],
         end: str | pd.Timestamp | tuple[pd.Timestamp, pd.Timestamp] | None = None,
         verbose: bool = False,
+        **kwargs,
     ) -> pd.DataFrame:
         return self._get_pricing_data(
             date,
@@ -123,14 +125,15 @@ class MISOAPI:
             market=Markets.REAL_TIME_HOURLY_EX_POST_PRELIM,
             prelim_or_final=PRELIMINARY_STRING,
             verbose=verbose,
+            **kwargs,
         )
 
-    @support_date_range(frequency="HOUR_START")
     def get_lmp_real_time_hourly_ex_post_final(
         self,
         date: str | pd.Timestamp | tuple[pd.Timestamp, pd.Timestamp],
         end: str | pd.Timestamp | tuple[pd.Timestamp, pd.Timestamp] | None = None,
         verbose: bool = False,
+        **kwargs,
     ) -> pd.DataFrame:
         return self._get_pricing_data(
             date,
@@ -139,14 +142,15 @@ class MISOAPI:
             market=Markets.REAL_TIME_HOURLY_EX_POST_FINAL,
             prelim_or_final=FINAL_STRING,
             verbose=verbose,
+            **kwargs,
         )
 
-    @support_date_range(frequency="5_MIN")
     def get_lmp_real_time_5_min_ex_ante(
         self,
         date: str | pd.Timestamp | tuple[pd.Timestamp, pd.Timestamp],
         end: str | pd.Timestamp | tuple[pd.Timestamp, pd.Timestamp] | None = None,
         verbose: bool = False,
+        **kwargs,
     ) -> pd.DataFrame:
         return self._get_pricing_data(
             date,
@@ -154,14 +158,15 @@ class MISOAPI:
             retrieval_func=self._get_lmp_real_time_5_min_ex_ante,
             market=Markets.REAL_TIME_5_MIN_EX_ANTE,
             verbose=verbose,
+            **kwargs,
         )
 
-    @support_date_range(frequency="5_MIN")
     def get_lmp_real_time_5_min_ex_post_prelim(
         self,
         date: str | pd.Timestamp | tuple[pd.Timestamp, pd.Timestamp],
         end: str | pd.Timestamp | tuple[pd.Timestamp, pd.Timestamp] | None = None,
         verbose: bool = False,
+        **kwargs,
     ) -> pd.DataFrame:
         return self._get_pricing_data(
             date,
@@ -170,14 +175,15 @@ class MISOAPI:
             market=Markets.REAL_TIME_5_MIN_EX_POST_PRELIM,
             prelim_or_final=PRELIMINARY_STRING,
             verbose=verbose,
+            **kwargs,
         )
 
-    @support_date_range(frequency="5_MIN")
     def get_lmp_real_time_5_min_ex_post_final(
         self,
         date: str | pd.Timestamp | tuple[pd.Timestamp, pd.Timestamp],
         end: str | pd.Timestamp | tuple[pd.Timestamp, pd.Timestamp] | None = None,
         verbose: bool = False,
+        **kwargs,
     ) -> pd.DataFrame:
         return self._get_pricing_data(
             date,
@@ -186,6 +192,7 @@ class MISOAPI:
             market=Markets.REAL_TIME_5_MIN_EX_POST_FINAL,
             prelim_or_final=FINAL_STRING,
             verbose=verbose,
+            **kwargs,
         )
 
     # NOTE: this method does not use the support_date_range decorator. Instead
@@ -206,6 +213,7 @@ class MISOAPI:
 
         return self._process_pricing_data(data_list, market=market)
 
+    @support_date_range(frequency="HOUR_START")
     def _get_lmp_day_ahead_hourly(
         self,
         date: datetime.datetime,
@@ -224,6 +232,7 @@ class MISOAPI:
 
         return data_list
 
+    @support_date_range(frequency="HOUR_START")
     def _get_lmp_real_time_hourly_ex_post(
         self,
         date: datetime.datetime,
@@ -242,6 +251,7 @@ class MISOAPI:
 
         return data_list
 
+    @support_date_range(frequency="5_MIN")
     def _get_lmp_real_time_5_min_ex_ante(
         self,
         date: datetime.datetime,
@@ -261,6 +271,7 @@ class MISOAPI:
 
         return data_list
 
+    @support_date_range(frequency="5_MIN")
     def _get_lmp_real_time_5_min_ex_post(
         self,
         date: datetime.datetime,
@@ -598,7 +609,10 @@ class MISOAPI:
             date = pd.Timestamp.now(tz=self.default_timezone).floor("d")
 
         return self._get_day_ahead_cleared_generation_hourly(
-            date, end, verbose, generation_type="physical"
+            date,
+            end,
+            verbose,
+            generation_type="physical",
         )
 
     @support_date_range(frequency="DAY_START")
@@ -612,7 +626,10 @@ class MISOAPI:
             date = pd.Timestamp.now(tz=self.default_timezone).floor("d")
 
         return self._get_day_ahead_cleared_generation_hourly(
-            date, end, verbose, generation_type="virtual"
+            date,
+            end,
+            verbose,
+            generation_type="virtual",
         )
 
     @support_date_range(frequency="DAY_START")
@@ -727,7 +744,10 @@ class MISOAPI:
             date = pd.Timestamp.now(tz=self.default_timezone).floor("d")
 
         return self._get_day_ahead_offered_generation_hourly(
-            date, end, verbose, ecotype="ecomax"
+            date,
+            end,
+            verbose,
+            ecotype="ecomax",
         )
 
     @support_date_range(frequency="DAY_START")
@@ -741,7 +761,10 @@ class MISOAPI:
             date = pd.Timestamp.now(tz=self.default_timezone).floor("d")
 
         return self._get_day_ahead_offered_generation_hourly(
-            date, end, verbose, ecotype="ecomin"
+            date,
+            end,
+            verbose,
+            ecotype="ecomin",
         )
 
     @support_date_range(frequency="DAY_START")
@@ -888,7 +911,7 @@ class MISOAPI:
     ) -> pd.DataFrame:
         if date == "latest":
             date = pd.Timestamp.today(tz=self.default_timezone).floor(
-                "d"
+                "d",
             ) - pd.Timedelta(days=1)  # Yesterday
 
         return self._get_real_time_cleared_demand(
@@ -951,15 +974,18 @@ class MISOAPI:
         The timestamps are off by 5 hours, seems to be a timezone issue, UTC instead of EST.
         """
         raise NotImplementedError(
-            "get_real_time_cleared_generation_hourly is not ready for use yet."
+            "get_real_time_cleared_generation_hourly is not ready for use yet.",
         )
         if date == "latest":
             date = pd.Timestamp.today(tz=self.default_timezone).floor(
-                "d"
+                "d",
             ) - pd.Timedelta(days=1)  # Yesterday
 
         return self._get_real_time_cleared_generation(
-            date, end, verbose, time_resolution=HOURLY_RESOLUTION
+            date,
+            end,
+            verbose,
+            time_resolution=HOURLY_RESOLUTION,
         )
 
     @support_date_range(frequency="DAY_START")
@@ -1155,7 +1181,7 @@ class MISOAPI:
     ) -> pd.DataFrame:
         if date == "latest":
             date = pd.Timestamp.now(tz=self.default_timezone).floor("d") - pd.Timedelta(
-                days=1
+                days=1,
             )
 
         date_str = date.strftime("%Y-%m-%d")
@@ -1194,7 +1220,7 @@ class MISOAPI:
 
         data = data.sort_values(["Interval Start", "Region"])
         return data[["Interval Start", "Interval End", "Region", "Load"]].reset_index(
-            drop=True
+            drop=True,
         )
 
     @support_date_range(frequency="DAY_START")
@@ -1254,7 +1280,7 @@ class MISOAPI:
         """
         if date == "latest":
             date = pd.Timestamp.now(tz=self.default_timezone).floor("d") + pd.Timedelta(
-                days=6
+                days=6,
             )  # Forecast date must be within 7 days of the publish_time.
 
         date_str = date.strftime("%Y-%m-%d")
@@ -1313,7 +1339,7 @@ class MISOAPI:
                 data[col] = data[col].astype(float)
 
         data = data.sort_values(
-            ["Interval Start", "Publish Time", "Region", "Local Resource Zone"]
+            ["Interval Start", "Publish Time", "Region", "Local Resource Zone"],
         )
         return data[
             [
